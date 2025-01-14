7f58f65b4b211de16d46f83d2a3c904b8544f18e
Shan k <shank@Shans-MacBook-Air.local>
2025-01-14 23:54:08 +0900

    Worked on the reser button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,7 +13,7 @@ export default function Card() {
     <div className="flex flex-col items-center justify-center w-[476px] h-[615px] bg-teal-900 rounded-lg transition-all shadow-[0_25px_121px_rgba(0,0,0,0.5)]">
       <Title />
       <Count count={count}/>
-      <ResetButton />
+      <ResetButton setCount={setCount}/>
       <CountButtons setCount={setCount}/>
     </div>)
 }
--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,8 +1,8 @@
 import { ResetIcon } from "@radix-ui/react-icons"
 
-export default function ResetButton() {
+export default function ResetButton({setCount}) {
   return (
-    <button className='px-4 py-2 rounded-lg mt-4 transition-all duration-200'>
+    <button  onClick={() => {setCount(0)}}  className='px-4 py-2 rounded-lg mt-4 transition-all duration-200'>
       <ResetIcon className="h-10 w-10  text-slate-100"/>
     </button>
   );
